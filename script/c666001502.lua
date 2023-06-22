9dd8089a5d5bee5182b7af09941422ffe3087765
Cxpherr <78995420+Cxpherr@users.noreply.github.com>
2023-06-22 02:33:00 -0500

    "Corvin Light Devourer" fix

---
+++ script/c666001502.lua
@@ -0,0 +1,45 @@
+--Corvin Light Devourer
+--Created by Aura, Coded by Voodoo
+local s,id=GetID()
+function s.initial_effect(c)
+	--Opponent sends 1 card from hand to Graveyard, if no cards in hand all opponent's monsters lose 2000 ATK.
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_HANDES+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,1-tp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--Effect
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	if #g>0 then
+		local sg=g:RandomSelect(1-tp,1)
+		Duel.SendtoGrave(sg,REASON_EFFECT)
+		if Duel.GetFieldGroupCount(e:GetHandlerPlayer(),0,LOCATION_HAND)==0 then
+			local c=e:GetHandler()
+			local ag=Duel.GetMatchingGroup(aux.FilterMaximumSideFunctionEx(aux.TRUE),tp,0,LOCATION_MZONE,nil)
+			for tc in ag:Iter() do
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_UPDATE_ATTACK)
+				e1:SetValue(-2000)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				tc:RegisterEffectRush(e1)
+			end
+		end
+	end
+end
